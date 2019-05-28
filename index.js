5ba5a063abaa5d220cc59e0939ba3e5df47b8478
Andres Fernando Lopez <alopeza@rentingcolombia.com>
2019-05-27 19:26:49 -0500

    Impresion de cursos con llamada erronea

--- index.js
+++ index.js
@@ -1,33 +1,36 @@
-const cursos = require('./ver_cursos');
-//onst opciones = require('./info_user')
+const opciones = require('./info_user')
+const cursos = require('./cursos').cursos;
+const ver_cursos = require('./ver_cursos');
 const fs = require('fs');
-const opciones = {
-    id: {
-        demand: true,
-        alias: 'i'
-    },
-    nombre_aspirante: {
-        demand: true,
-        alias: 'n'
-    },
-    cedula: {
-        demand: true,
-        alias: 'c'
-    }
-}
+
 const argv = require('yargs')
     .command('inscribir', 'inscribir aspirante a uno de los cursos', opciones)
     .argv
 
-//console.log(ver_cursos.imprimir_cursos());
+let programa = argv._[0];
+console.log('El valor de programa es: ' + programa);
 
-let comand = argv._[0];
-
-console.log('El comando ingresado es: ' + argv.c);
-
-switch (comand) {
+switch (programa) {
     case 'inscribir':
-        if (Number(argv.id) < cursos.length + 1) {
+            console.log('Ingresa a el if: ' + argv.i + ' tamaño: '+ cursos.length);
+        if (Number(argv.i) < cursos.length + 1) {
+            name = cursos.find(cursoEst => cursoEst.id == argv.i)
+            
+        } else {
+            console.log(`El valor introducido en "${argv.id}" no es número válido`)
+        }
+        break;
+};
+
+if (!programa){
+    console.log('ingreso a other');
+    ver_cursos.imprimir_cursos();
+};
+
+/*switch (programa) {
+    case 'inscribir':
+            console.log('Ingresa a el if: ' + argv.i + ' tamaño: '+ cursos.length);
+        if (Number(argv.i) < cursos.length + 1) {
             name = cursos.find(cursoEst => cursoEst.id == argv.id)
             crearArchivo(argv.nombre, argv.cedula, ms(name.cursos, name.horas, name.valor))
                 .then(e => console.log('Se ha creado el archivo'))
@@ -36,17 +39,4 @@ switch (comand) {
             console.log(`El valor introducido en "${argv.id}" no es número válido`)
         }
         break;
-}
-
-if (!comand)
-    for (var trav in cursos) {
-        mensaje(trav);
-    }
-
-function mensaje(id_curso) {
-    setTimeout(function() {
-        let texOut = 'El curso se llama';
-        let name = cursos[id_curso];
-        console.log(texOut + ms(name.cursos, name.horas, name.valor));
-    }, id_curso * 2000 + 2000);
-}
+};*/
